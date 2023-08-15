75b5eb97462ded98d2f707065ed4ee826e8c76cf
Rushi_0508 <rushigandhi14@gmail.com>
2023-08-15 11:14:09 +0530

    fix: cleared states after request

--- frontend/src/components/Items.tsx
+++ frontend/src/components/Items.tsx
@@ -116,6 +116,7 @@ export default function Items() {
             toast.success("Item added successfully")
         }
         reset();
+        setItemTags([])
         setIsLoading(false);
         setItemDialog(false);
         fetchItems(sort,filterTags,search);
