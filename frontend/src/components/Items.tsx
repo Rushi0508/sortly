d99e72960e4af2f71b17430240522285cab83d25
Rushi_0508 <rushigandhi14@gmail.com>
2023-09-15 18:15:18 +0530

    feat: added page loaders

--- frontend/src/components/Items.tsx
+++ frontend/src/components/Items.tsx
@@ -204,13 +204,13 @@ export default function Items() {
     return (
         <Layout>
             {!items? 
-            // <div className='h-[90vh] bg-[#f3f4f6] flex justify-center items-center'>
-            //     <svg style={{width: "2rem", height: "2rem" }} className="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            //         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            //         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            //     </svg> 
-            // </div>
-            null
+            <div className='bg-[#f3f4f6] flex flex-1 justify-center items-center'>
+                <svg style={{width: "2rem", height: "2rem" }} className="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg> 
+            </div>
+            // null
             : (
             <>
                 <div className='bg-[#f3f4f6] flex-1 w-full'>
@@ -336,7 +336,7 @@ export default function Items() {
                             </Transition>
                         </Menu>
                     </div>
-                    {items.length===0? <p className='mt-5 text-xl h-screen text-center'>No Items found</p>:
+                    {items.length===0? <p className='mt-5 text-xl flex-1 text-center'>No Items found</p>:
                     <div className='p-5 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 md:p-8 justify-center justify-items-center grid gap-4 sm:gap-3'>
                         {items.map((item,index)=>{
                             return <ItemCard 
